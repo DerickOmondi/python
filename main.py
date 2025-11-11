aa06c0606e8f04e2e474a403cdfda89d1a39fc2b
DERICK MAUWA <omondiderick950@gmail.com>
2025-11-11 03:14:04 +0300

    commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# print("Hello, World!")
+# # variables
+# age = 24
+# name = "dero"
+
+# print(age) 
+
+# num1 = float(input("Enter your first number "))
+# num2 = float(input("Enter your second number number "))
+
+# total = (num1 + num2)
+
+# print(f"The total is  {total}")
+
+# calculate area of circle,radius is an input from the user
+
+import math
+pi = math.pi
+# print(pi)
+
+R = float(input("Enter the radius of the circle "))
+
+area = round(float((pi * R **2)) , 5)
+
+print(f"The area of the circle is  {area}")
