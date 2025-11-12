5642d3d86cb714da529c4a5d2f108cdec3802de2
DERICK MAUWA <omondiderick950@gmail.com>
2025-11-13 00:15:36 +0300

    conditional operatores and data structure

--- conditional.py
+++ conditional.py
@@ -1,3 +1,38 @@
 #  if else
 
-score = 0
+# score = input(int("Input your marks"))
+
+# if score >=80 and score <= 100:
+#     print("Excellent")
+
+# elif score >=70 and score < 80:
+#     print("Good")
+
+# elif score >=60 and score < 70:
+#     print("pass")
+
+# elif score >=50 and score < 60:
+#     print("fair")
+
+# elif score >=40 and score < 50:
+#     print("repeat")
+
+# elif score < 40:
+#     print("fail")
+
+# else:
+#     print("Enter a number between 0 and 100")
+
+
+# while loop
+
+# i = 0 
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+# for loop
+
+# for y in 'python':
+#     print(y)
+
